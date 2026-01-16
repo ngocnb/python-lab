821b5ada91efc2c6c6184234efb8bd35255eaa03
Nguyen Bao Ngoc <ngocnguyenbao2707@gmail.com>
2026-01-16 14:08:59 +0700

    restructure code

--- my_math/basic.py
+++ my_math/basic.py
@@ -4,17 +4,20 @@ def my_abs(x):
     """
     pass
 
+
 def my_trunc(x):
     """
     Returns x truncated to an integer (towards 0).
     """
     pass
 
+
 def my_floor(x):
     """
     Returns the largest integer <= x.
     """
     pass
+
 
 def my_ceil(x):
     """
